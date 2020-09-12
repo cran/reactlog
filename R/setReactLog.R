6cff68820187e584a0f456f205fd8331df6da431
Barret Schloerke <barret@rstudio.com>
2020-09-12 12:20:03 +0000

    version 1.1.0

---
+++ R/setReactLog.R
@@ -0,0 +1,26 @@
+#' Enable or disable the reactlog
+#'
+#' Before the reactlog can be visualized, it needs to be enabled. For security
+#' and performance reasons, you should not enable the reactlog in a shiny app
+#' in production.
+#' @param warn Should a warning message be shown?
+#' @seealso [reactlog::reactlog_show()]
+#' @name setReactLog
+#' @export
+#' @rdname setReactLog
+reactlog_enable <- function() {
+  options(shiny.reactlog = TRUE)
+}
+
+#' @export
+#' @rdname setReactLog
+reactlog_disable <- function(warn = TRUE) {
+  if (!isTRUE(warn) && !identical(warn, FALSE)) {
+    stop("'warn' must be either 'TRUE' or 'FALSE'")
+  }
+
+  options(shiny.reactlog = FALSE)
+  if (isTRUE(warn)) {
+    warning("Please restart R to free up 'reactlog' memory", call. = FALSE)
+  }
+}
